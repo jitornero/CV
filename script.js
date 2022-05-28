aefa923653beef32caad3a598932b750514cb481
jitornero <tornerojuan@gmail.com>
2022-05-28 20:10:13 -0300

    1er event de border del primer card ok

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 
 
-const tile1 = document.querySelector('.p-card');
+const tile1 = document.querySelector('#c1');
+console.log(tile1);
 
 
-tile1.addEventListener('', function () {
 
-    
-}
+
+
+tile1.addEventListener("mouseover", function(){
+
+    tile1.style.borderColor = 'black';
+});
+
+
+
+tile1.addEventListener("mouseout", function(){
+
+    tile1.style.borderColor = '';
+});
